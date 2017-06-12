d62483af72b344a5a9bc9f214ee1f40869b485b4
Kurt L. Manion <KLManion@gmail.com>
2017-06-12 04:11:47 -0400

    changed the install directory for the manpage to /usr/local/share/man/man1/

--- install.sh
+++ install.sh
@@ -9,12 +9,17 @@ fi
 
 if [ ! -d "/usr/local/bin/" ]; then
 	mkdir -p "/usr/local/bin/"
-	[ $? -ne 0 ] && exit 1;
+	test $? -ne 0 && exit 1;
+fi
+
+if [ ! -d "/usr/local/share/man/man1/" ]; then
+	mkdir -p "/usr/local/share/man/man1/"
+	test $? -ne 0 && exit 1;
 fi
 
 chmod +x "./src/dwn.sh"
 cp "./src/dwn.sh" "/usr/local/bin/dwn"
-cp "./share/man/man1/dwn.1" "/usr/share/man/man1/"
+cp "./share/man/man1/dwn.1" "/usr/local/share/man/man1/"
 exit $?;
 
 # vim: set ts=4 sw=4 noexpandtab:
