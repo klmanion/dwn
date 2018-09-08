fb4c7638ae1c9f26f00ba6fcf9a1f6727dc26fba
Kurt L. Manion <klmanion@gmail.com>
2018-09-08 15:01:18 -0400

    changed default behavior to printing

--- src/dwn.sh
+++ src/dwn.sh
@@ -15,21 +15,22 @@ version="3.0.1"
 #on kali linux the stat version's first line is
 #stat (GNU coreutils) 8.25
 
-declare r_flg=0
-declare l_flg=0
-declare m_flg=0
-declare mv_flgs="-n"
+declare cmd=""
+declare cmd_flgs=""
+declare cmd_post=""
+
 declare num_files=1
+
 declare name="`basename "${0:-dwn}"`"
 
 usage() {
-	printf '%s%s\n%s\n%s%s\n%s%s\n' 					\
-		'usage: '"$name"' -- return path of file most recently '	\
+	printf '%s%s\n%s\n%s%s\n%s%s\n' 				\
+		'usage: '"$name"' -- return path of file most recently '\
 			'added to a folder' 				\
 		"$name"' [-r] [-d directory] [-n num_files]'  		\
-		"$name"' [-r] [-d directory] [-o application] '		\
-			'[-f flags] [-n num_files]'			\
-		"$name"' [-r] [-d directory] [-m destination | -M] '	\
+		"$name"' [-d directory] [-o application] [-f flags] '	\
+			'[-n num_files]'				\
+		"$name"' [-d directory] [-m destination | -M] '		\
 			'[-f flags] [-n num_files]'
 	exit 64;
 }
@@ -46,67 +47,69 @@ err() {
 	exit 65;
 }
 
-while getopts ":d:a:rlm:Mfin:hV" opt "$@"; do
+while getopts ":d:rn:fm:MohV" opt "$@"; do
 	case "$opt" in
-		(d)
-			if [[ ${OPTARG:0:1} == "~" ]]; then
-				dir="$HOME""${OPTARG:1}"
-			elif [[ ${OPTARG:0:1} =~ "\." ]]; then
-				dir=`pwd`/"$OPTARG"
-			else
-				dir="$OPTARG"
-			fi
-			dir="`echo "$dir" | sed -e 's/\*$//; s/\/$//'`"
-			test ! -d "$dir" && err 'directory does not exist'
-			;;
-		(a)
-			app_path="`echo "$2" | sed -e '\
-				/^[~\.\/]/ !s/^/&\/Applications\//
-				/\.app$/ !s/$/\.app/'`"
-			test ! -x "$app_path" && err 'specified application is not executable'
-			;;
-		(r)
-			r_flg=1
-			;;
-		(l)
-			l_flg=1
-			break;
-			;;
-		(m)
-			mv_dest="$OPTARG"
-			m_flg=1
-			;;
-		(M)
-			mv_dest="$PWD"
-			m_flg=1
-			;;
-		(f)
-			mv_flgs="-f"
-			;;
-		(i)
-			mv_flgs="-i"
-			;;
-		(n)
-			num_files="$OPTARG"
-			test -n "`echo "$num_files" | sed -e 's/[0-9]*//'`" && \
-				err '-n takes only numeric arguments'
-			test "$num_files" -lt 1 && \
-				err 'number of files must be set to at least 1' 
-			;;
-		(h)
-			usage
-			;;
-		(V)
-			version
-			;;
-		(\?)
-			err 'unknown argument '"$OPTARG"''
-			;;
+	(d)
+		if [[ x${OPTARG:0:1} == x"~" ]]; then
+			dir="$HOME""${OPTARG:1}"
+		elif [[ x${OPTARG:0:1} =~ x"\." ]]; then
+			dir=`pwd`/"$OPTARG"
+		else
+			dir="$OPTARG"
+		fi
+		dir="`echo "$dir" | sed -e 's/\*$//; s/\/$//'`"
+		test ! -d "$dir" && err 'directory does not exist'
+		;;
+	(r)
+		cmd=""
+		cmd_flgs=""
+		cmd_post=""
+		;;
+	(n)
+		num_files="$OPTARG"
+		test -n "`echo "$num_files" | sed -e 's/[0-9]*//'`" && \
+			err '-n takes only numeric arguments'
+		test "$num_files" -lt 1 && \
+			err 'number of files must be set to at least 1' 
+		;;
+	(f)
+		cmd_flgs="$cmd_flgs $OPTARG"
+		;;
+	(m)
+		cmd="mv"
+
+		cmd_post="$OPTARG"
+		;;
+	(M)
+		cmd="mv"
+
+		cmd_flgs="$cmd_flgs -n"
+
+		cmd_post="$PWD"
+		;;
+	(o)
+		if [[ x"$OSTYPE" == x"linux-gnu" ]]; then
+			cmd="xdg-open"
+		elif [[ x"$OSTYPE" == x"darwin*" ]]; then
+			cmd="open"
+		else
+			cmd="open"
+		fi
+		;;
+	(h)
+		usage
+		;;
+	(V)
+		version
+		;;
+	(\?)
+		err 'unknown argument '"$OPTARG"''
+		;;
 	esac
 done
 shift $((OPTIND-1))
 
-test $# -gt 0 -a $l_flg -eq 0 && err 'extraneous arguments'
+test $# -gt 0 && err 'extraneous arguments'
 
 : ${dir:="$HOME/Downloads"}
 
@@ -121,32 +124,12 @@ filepath_lst="`eval "$stat_cmd" | sort -rn | head -$num_files \
 IFS=$'\t'
 read -r -a filepath_arr <<< "$filepath_lst"
 
-if [[ x"$OSTYPE" == x"linux-gnu" ]]; then
-	open_cmd="xdg-open"
-elif [[ x"$OSTYPE" == x"darwin*" ]]; then
-	open_cmd="open"
-else
-	open_cmd="open"
-fi
-
 for filepath in "${filepath_arr[@]}"; do
 	test -z "$filepath" && err 'stat command failed'
-	#test -d "$filepath" && $open_cmd -R "$filepath"
-	test $m_flg -eq 1 && { mv "$filepath" "$mv_dest"; continue; }
-	test $r_flg -eq 1 && { echo "$filepath"; continue; }
-	test ! -r "$filepath" && err 'most recently downloaded file is unreadable'
 
-	if [ $l_flg -eq 1 ]; then
-		test -n "$app_path" \
-			&& $open_cmd -a "$app_path" "$filepath" \
-			|| $open_cmd "$@" "$filepath"
-	elif [ $m_flg -eq 1 ]; then
-		mv "$mv_flgs" "`dwn -rd $dir`" "$mv_dest"
-	else
-		test -n "$app_path" \
-			&& $open_cmd -a "$app_path" "$filepath" \
-			|| $open_cmd "$filepath"
-	fi
+	test -z "$cmd" \
+		&& echo "$filepath" \
+		|| $cmd "$cmd_flgs" "$filepath" "$cmd_post"
 done
 
 exit 0;
