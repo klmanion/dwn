c66e06587848ceb88cc073811f8080285635453e
Kurt L. Manion <klmanion@gmail.com>
2019-02-08 16:27:57 -0500

    updated install script to use install program rather than doing it by hand

--- install.sh
+++ install.sh
@@ -2,24 +2,20 @@
 #install.sh
 # install dwn and its manpage
 
+if [ ! -d "$PWD/src/" ]; then
+	printf 'this script must be run in the top-level directory' >&2
+	exit 1;
+fi
+
 if [ $(id -u) -ne 0 ]; then
 	printf 'this script must be run as root\n' >&2
 	exec sudo "$0" $*
 fi
 
-if [ ! -d "/usr/local/bin/" ]; then
-	mkdir -p "/usr/local/bin/"
-	test $? -ne 0 && exit 1;
-fi
+install -dv '/usr/local/bin/'
+install -Cv "$PWD/src/dwn.sh" '/usr/local/bin/dwn'
+install -dv '/usr/local/share/man/man1/'
+install -Cv "$PWD/share/man/man1/dwn.1" '/usr/local/share/man/man1/dwn.1'
+exit $?
 
-if [ ! -d "/usr/local/share/man/man1/" ]; then
-	mkdir -p "/usr/local/share/man/man1/"
-	test $? -ne 0 && exit 1;
-fi
-
-chmod +x "./src/dwn.sh" || exit 1;
-cp -f "./src/dwn.sh" "/usr/local/bin/dwn" || exit 1;
-cp -f "./share/man/man1/dwn.1" "/usr/local/share/man/man1/"
-exit $?;
-
-# vim: set ts=8 sw=8 noexpandtab tw=79:
+# vi: set ts=8 sw=8 noexpandtab tw=79:
